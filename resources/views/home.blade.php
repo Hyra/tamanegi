7a4f66353997e1136f8e6cff6b5f0c29c90b5450
Stef van den Ham <stef@mindthecode.com>
2021-05-28 19:45:08 +0200

    Homepage

---
+++ resources/views/home.blade.php
@@ -0,0 +1,7 @@
+@extends('layouts.default')
+
+@section('content')
+    <div class="flex items-center justify-center h-screen">
+        <h1 class="text-5xl font-bold">Homepage</h1>
+    </div>
+@endsection
