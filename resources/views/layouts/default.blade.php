29fc685d74e4454012f77427197e9ecf499380f2
Stef van den Ham <stef@mindthecode.com>
2021-05-29 12:00:56 +0200

    Adds GTM

--- resources/views/layouts/default.blade.php
+++ resources/views/layouts/default.blade.php
@@ -12,6 +12,21 @@
 
 <body class="antialiased">
     @yield('content')
+
+    <script src="{{ asset('js/app.js') }}" async></script>
+    <script async src="https://www.googletagmanager.com/gtag/js?id={{ env('GTM_ID') }}"></script>
+    <script>
+        window.dataLayer = window.dataLayer || [];
+
+        function gtag() {
+            dataLayer.push(arguments);
+        }
+        gtag('js', new Date());
+
+        gtag('config', 'UA-40203772-4');
+
+    </script>
+
     @livewireScripts
 </body>
 
